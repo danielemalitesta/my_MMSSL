2745f64c0a86041da663ba0f5d55be26a040686e
dmalitesta <d.malitesta@gmail.com>
2023-07-14 15:42:56 +0200

    First commit.

--- MMSSL/run_all.sh
+++ MMSSL/run_all.sh
@@ -18,6 +18,7 @@ do
   do
     for ran in ${random[@]};
     do
+      echo $str $sam $ran
       if [[ $str == feat_prop ]]
       then
         python3.8 main.py --dataset $1 --strategy $str --feat_prop co --masked_items_image ./data/$1/$sam"_"$ran.txt --masked_items_text ./data/$1/$sam"_"$ran.txt
